902e845c7b23bd7e317176f87005c007717fd0ba
Pinja <97978991+pinjama@users.noreply.github.com>
2025-01-22 14:24:30 +0200

    Add files via upload

---
+++ Summa/Tehtava1b/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Oletko tekemässä yhteen- vai vähennyslaskua? (+/-)");
+var Lasku = Console.ReadLine();
+if (Lasku == "+")
+{
+    Console.WriteLine("Anna luku 1");
+    double luku1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Anna luku 2");
+    double luku2 = Convert.ToDouble(Console.ReadLine());
+    double luku3 = luku1+luku2;
+    Console.WriteLine("Summa on: " + luku3);
+}
+else if (Lasku == "-")
+{
+    Console.WriteLine("Anna luku 1");
+    double luku1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Anna luku 2");
+    double luku2 = Convert.ToDouble(Console.ReadLine());
+    double luku3 = luku1 - luku2;
+    Console.WriteLine("Summa on: " + luku3);
+}
